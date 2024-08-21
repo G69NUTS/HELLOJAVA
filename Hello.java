e94b664e432f48e5f4d2c88daf2b96696aac3d40
G69NUTS <gutsonikuma@gmail.com>
2024-08-21 17:08:29 -0500

    Read a number

--- Hello.java
+++ Hello.java
@@ -1,10 +1,22 @@
-class Hello{
+import java.util.Scanner;
+class Hello {
 
-public static void main(String[] args){
-
-System.out.println("Hello desde el Block De Notas");
-
-}
+public static  void main (String[] args) {
 
 
-}
+Scanner leer = new Scanner(System.in);
+
+System.out.println("Hello desde block notas"); 
+String name;
+System.out.println("type your name: ");
+name = leer.next();
+System.out.println(name);
+System.out.println("type a number");
+int number = leer.nextInt();
+System.out.println("type another number");
+int number2 = leer.nextInt();
+System.out.println(number+number2);
+System.out.println(number-number2);
+System.out.println(number*number2);
+	}
+} 
